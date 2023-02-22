c1b977872b90e70ad9cf0b20351a319cc0ffa373
fernandocarlosantos <78769221+fernandocarlosantos@users.noreply.github.com>
2023-02-21 21:47:19 -0300

    Delete create_user_local.ps1

--- create_user_local.ps1
+++
@@ -1,4 +0,0 @@
-$password = Read-Host -AsSecureString
-
-New-LocalUser -Name "local.fernando" -Description "Fernando Correia" -AccountNeverExpires -PasswordNeverExpires -Password $password 
-Add-LocalGroupMember -Group Administrators -Member local.fernando
